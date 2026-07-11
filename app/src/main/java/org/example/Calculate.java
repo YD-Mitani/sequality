b93b90ea737dac3ffae567e67ce3f94d2be1a4ff
yudai <m1m02530164@oit.ac.jp>
2026-07-11 23:01:51 +0900

    課題完成

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,25 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public int getIndividualFee(int age) {
+    int money = 0;
+    if (age <= 12) {
+      money = 1000;
+    } else {
+      money = 2000;
+    }
+    return money;
+  }
+
+  public int getTotalFee(int age, int groupSize) {
+    int total = 0;
+    if (groupSize < 10) {
+      total = getIndividualFee(age) * groupSize;
+    } else {
+      total = getIndividualFee(age) * groupSize * 9 / 10;
+    }
+    return total;
+  }
+
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,52 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testGetIndividualFee_Age10() {
+    Calculate calc = new Calculate();
+    int expected = 1000;
+
+    assertEquals(expected, calc.getIndividualFee(10));
+  }
+
+  @Test
+  public void testGetIndividualFee_Age25_Returns2000() {
+    Calculate calc = new Calculate();
+    int expected = 2000;
+
+    assertEquals(expected, calc.getIndividualFee(25));
+  }
+
+  @Test
+  public void testGetTotalFee_Age10AndSize1() {
+    Calculate calc = new Calculate();
+    int expected = 1000;
+
+    assertEquals(expected, calc.getTotalFee(10, 1));
+  }
+
+  @Test
+  public void testGetTotalFee_Age25AndSize1() {
+    Calculate calc = new Calculate();
+    int expected = 2000;
+
+    assertEquals(expected, calc.getTotalFee(25, 1));
+  }
+
+  @Test
+  public void testGetTotalFee_Age10AndSize15() {
+    Calculate calc = new Calculate();
+    int expected = 13500;
+
+    assertEquals(expected, calc.getTotalFee(10, 15));
+  }
+
+  @Test
+  public void testGetTotalFee_Age25AndSize15() {
+    Calculate calc = new Calculate();
+    int expected = 27000;
+
+    assertEquals(expected, calc.getTotalFee(25, 15));
+  }
 }
